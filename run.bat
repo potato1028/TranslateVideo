001575f44b1f80493ae6e407284f1fcdf57d4622
potato1028 <ghgh10288@naver.com>
2026-03-23 19:08:04 +0900

    실행방법, 환경설정 추가

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+echo 1. 가상 환경을 확인 및 설정하는 중입니다...
+if not exist venv (
+    python -m venv venv
+)
+
+echo 2. 가상 환경을 활성화하고 필요한 모듈을 설치합니다...
+call venv\Scripts\activate
+pip install -r requirements.txt
+
+echo 3. 프로그램을 실행합니다...
+server.py
+
+pause
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "1. 가상 환경을 확인 및 설정하는 중입니다..."
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+echo "2. 가상 환경을 활성화하고 필요한 모듈을 설치합니다..."
+source venv/bin/activate
+pip install -r requirements.txt
+
+echo "3. 프로그램을 실행합니다..."
+python3 server.py
